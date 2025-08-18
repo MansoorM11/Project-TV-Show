894176cf61f5ba1d3ec9ba0d6ad4dd81bbf8b616
Mansoor Munawar <mansoor.munawar448@gmail.com>
2025-08-18 15:19:40 +0100

    Refactor episode rendering logic in script.js and add footer to index.html

--- script.js
+++ script.js
@@ -6,7 +6,24 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.textContent = ``;
+  episodeList.forEach((episode) => {
+    let episodeCard = document.createElement("section");
+    episodeCard.className = "episode";
+    let title = document.createElement("h2");
+    title.textContent = episode.name;
+    let seasonAndEpisode = document.createElement("p");
+    let seasonPadded = episode.season.toString().padStart(2, "0");
+    let episodePadded = episode.number.toString().padStart(2, "0");
+    seasonAndEpisode.innerHTML = `S${seasonPadded}E${episodePadded}`;
+    let image = document.createElement("img");
+    image.src = episode.image.medium;
+
+    let summary = document.createElement("div");
+    summary.innerHTML = `${episode.summary}`;
+    episodeCard.append(title, seasonAndEpisode, image, summary);
+    rootElem.append(episodeCard);
+  });
 }
 
 window.onload = setup;
